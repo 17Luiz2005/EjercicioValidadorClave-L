6a28bed603e4b7ba86d068ab2d8b958f0b640482
Luis Anaya <anayarodriguez1961@gmail.com>
2024-10-17 12:45:38 -0500

    creacion de la clase abstracta "ReglaValidacion" y su metodo abstracto "es_valida", y la clase "_validar_longitud"

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,11 @@
 # TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion(ABC):
+    @abstractmethod
+    def es_Valida(self, clave: str)->bool:
+        pass
+
+    def _validar_longitud(clave: str)->bool:
+        pass
