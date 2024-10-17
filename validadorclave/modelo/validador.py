f0334c0d846bce96de8f07f81d8f91d5f0c7bc25
Luis Anaya <anayarodriguez1961@gmail.com>
2024-10-17 13:22:45 -0500

    creacion de todos los metodos de la clase 'ReglaValidacion'

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -3,9 +3,23 @@ from abc import ABC, abstractmethod
 
 
 class ReglaValidacion(ABC):
+    def __init__(self, _longitud_esperada: int):
+        self._longitud_esperada = _longitud_esperada
+
+    def _validar_longitud(self, clave:str)->bool:
+        return len(clave) > self._longitud_esperada
+    
+    def _contiene_mayuscula(self, clave: str)->bool:
+        return any(c.isupper() for c in clave)
+
+    def _contiene_mayusculas(self, clave: str)->bool:
+        return any(c.islower() for c in clave)
+    
+    def _contiene_numero(self, clave: str)->bool:
+        return any(c.isdigit() for c in clave)
+
+
     @abstractmethod
-    def es_Valida(self, clave: str)->bool:
+    def es_valida(self, clave: str)->bool:
         pass
 
-    def _validar_longitud(clave: str)->bool:
-        pass
